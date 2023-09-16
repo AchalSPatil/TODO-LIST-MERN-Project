6c55b310e8dee8fc5813667602c3c288f8ee2216
AchalSPatil <120000810+AchalSPatil@users.noreply.github.com>
2023-09-16 12:20:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,42 +6,16 @@ import './App.css';
 import Header from './components/header';
 import TodoForm from './components/TodoForm';
 import Todos from './components/Todos';
-// import Login from "./components/login/login"
-// import Register from "./components/register/register"
-// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-// import { useEffect, useState } from 'react';
+
 
 function App() {
-  // const [ user, setLoginUser] = useState({})
-
-  // useEffect(() => {
-  //   setLoginUser(JSON.parse(localStorage.getItem("MyUser")))
-  // }, [])
-
-  // const updateUser = (user) => {
-  //   localStorage.setItem("MyUser", JSON.stringify(user))
-  //   setLoginUser(user)
-  // }
+ 
   return (
     <div>
       <Header />
       <TodoForm />
       <Todos />
-      {/* <Router>
-        <Switch>
-          <Route exact path="/">
-            {
-              user && user._id ? <TodoForm updateUser={updateUser} /> : <Login updateUser={updateUser}/>
-            }
-          </Route>
-          <Route path="/login">
-            <Login updateUser={updateUser}/>
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-        </Switch>
-      </Router> */}
+      
     </div>
   );
 }
